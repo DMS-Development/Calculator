f76ac7e152b2354c4913cffe96177c6d10cba58f
ShimenkoDev <davidshimenko@gmail.com>
2022-02-26 20:57:02 -0700

    Cleaned up CSS a bit, wrote basic arithmetic functions for the calculator to use and tested in console

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.item');
+//const display = document.querySelector('.display');
+//const buttons = document.querySelectorAll('.item');
+const addition = (firstNumber, secondNumber) => sum = firstNumber + secondNumber;
+const subtraction = (firstNumber, secondNumber) => minuend = firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => product = firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => dividend = firstNumber / secondNumber;
+const operate = (firstNumber, operator, secondNumber) => {
+    if (operator === '+') {
+        addition(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        subtraction(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        divide(firstNumber, secondNumber);
+    }
+};
+
+console.log(operate(10, '/', 400));
